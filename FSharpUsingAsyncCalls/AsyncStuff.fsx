5425ecb55cb3c725f49cc1b6a308605ed589af00
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2021-04-24 20:53:54 +0100

    some rules

---
+++ FSharpUsingAsyncCalls/AsyncStuff.fsx
@@ -0,0 +1,23 @@
+﻿(*
+async { <- computational expression
+        !               ->              Bang
+        !               ->              always work with async
+        no !            ->              no async
+
+        let x = functionOrFunctionCallOrValue            ->             normal value binding
+        let! x = functionOrFunctionCallOrValueASYNC      ->             awaits the async Operation to complete
+
+        use x = functionOrFunctionCallOrValue            ->             normal value binding with automatic disposal
+        use! x = functionOrFunctionCallOrValueASYNC      ->             awaits the async operation to bind with automatic disposal
+
+        do              ->              calls operation that returns the unit
+        do!             ->              awaits the called operation to return the unit
+
+        match functionOrFunctionCallOrValue with         ->             normal match
+        match! functionOrFunctionCallOrValueASYNC with   ->             only after the async operation finishes that it commences matching
+
+        return          ->              if this return is in an async block, it wraps the normal value in an Async object example: async {return 22} -> Async<int>
+        return!         ->              returns an async without wrapping it: e.g async {return! functionOrFunctionCallOrValueASYNC } -> Async<int>.
+                                        if we didnt include the !, the output would wrap the already wrapped async function so it would look like this: Async<Async<int>>
+   }
+*)
